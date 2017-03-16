b60b0c2a7c562149c7e17e0966672604494f1b1d
Abunuo <liangmingqing315@163.com>
2017-03-16 10:41:51 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+var path = require('path');
+var webpack = require('webpack');
+var js_dist = path.join(__dirname, './dist');
+
+module.exports = {
+  name: 'chartComponent',
+  entry: {
+    line: './line.jsx',
+  },
+  output: {
+    path: js_dist,
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx$/],
+        loaders: ["babel-loader"],
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.js', '.jsx']
+  }
+};
